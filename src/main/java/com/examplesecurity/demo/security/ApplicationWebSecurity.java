6d7d057f0faf4b10d7aa7e749a3304a2e2b2decc
zakaria5729 <zakariahossain143@gmail.com>
2021-08-18 02:06:46 +0600

    ant matcher

--- src/main/java/com/examplesecurity/demo/security/ApplicationWebSecurity.java
+++ src/main/java/com/examplesecurity/demo/security/ApplicationWebSecurity.java
@@ -13,6 +13,8 @@ public class ApplicationWebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "/css/*", "/js/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
